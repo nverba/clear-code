7fc18f2e4f0b3b40e6416cf3d51edd7c518e5d51
nverba <distantstate@gmail.com>
2014-08-12 15:09:18 +0100

    removed unneeded session storage

--- app/code-respect-inject.js
+++ app/code-respect-inject.js
@@ -1,6 +1,7 @@
 if (!document.getElementById('code-respect-injected')) {
+
   chrome.runtime.sendMessage({ codeRespectRequest: "codeRespectInject" }, function(response) {
-    sessionStorage.codeRespectInjected = true;
+
     var div = document.createElement('div');
     div.id  = 'code-respect-injected';
     document.body.appendChild(div);
