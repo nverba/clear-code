8f26e253814fbaa8d079f16edb14b5873dfaa375
nverba <distantstate@gmail.com>
2014-08-12 16:12:21 +0100

    basic display of code source

--- app/code-respect.js
+++ app/code-respect.js
@@ -1,11 +1,20 @@
 var elements = document.getElementsByTagName("pre");
+var pre  = document.createElement('pre');
+var code = document.createElement('code');
+
+pre.id  = 'code-respect-pre';
+pre.style.display = 'none';
+code.id = 'code-respect-code';
+pre.appendChild(code);
+document.body.appendChild(pre);
+
+function openCode(element) {
+  code.innerText = js_beautify(element.innerText);
+  pre.style.display = 'block';
+}
 
 for (var i=0, max=elements.length; i < max; i++) {
   injectButton(elements[i]);
-}
-
-function openCode(element) {
-
 }
 
 function injectButton(element) {
@@ -13,6 +22,9 @@ function injectButton(element) {
   var button = document.createElement('button');
   button.style.backgroundImage = 'url(' + chrome.extension.getURL("icons/icon19.png") + ')';
   button.className = 'code-respect';
+  button.onclick = function () {
+    openCode(element);
+  };
   div.appendChild(button);
   element.appendChild(div);
 }
