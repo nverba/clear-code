a91e9c2383236fc64c10c346d531bac9936bac87
nverba <distantstate@gmail.com>
2014-08-15 20:49:59 +0100

    fixed wrong css link id

--- app/re-view-options.js
+++ app/re-view-options.js
@@ -1,6 +1,6 @@
 var style = document.getElementById('hljs-style');
 
-chrome.storage.local.get({ 'highlight_css_link': 'tomorrow-night' }, function (response) {
+chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
   style.value = response.highlight_css_link;
 });
 
