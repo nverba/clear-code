71bcd68682a7a0a36cc9c10d39a868b9acb1d36c
nverba <distantstate@gmail.com>
2014-08-17 15:09:03 +0100

    angular controlling style selector

--- app/options/clear-code-options.js
+++ app/options/clear-code-options.js
@@ -1,16 +1,40 @@
 var Options = angular.module('Options', []);
-var css     = document.getElementById('highlight-css-link');
-var code    = document.getElementById('code');
-var style   = document.getElementById('hljs-style');
+
+Options.controller('HeadController', function HeadController($scope) {
+
+  chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
+    $scope.$apply(function () {
+      $scope.highlighterStyle = { url: chrome.extension.getURL('highlight/styles/default.css') } ;
+    });
+  });
+
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if (!changes.highlight_css_link) { return; }
+
+    $scope.$apply(function () {
+      $scope.highlighterStyle.url = chrome.extension.getURL('highlight/styles/' + changes.highlight_css_link.newValue);
+    });
+  });
+});
 
 Options.controller('OptionsController', function OptionsController($scope) {
 
   $scope.menu = ['Highlighter', 'Beautifier'];
   $scope.name = { active: $scope.menu[0] };
+  $scope.options = ['arta.css', 'atelier-heath.light.css', 'codepen-embed.css', 'github.css', 'magula.css', 'pojoaque.css', 'solarized_light.css', 'vs.css', 'ascetic.css', 'atelier-lakeside.dark.css', 'color-brewer.css', 'googlecode.css', 'mono-blue.css', 'sunburst.css', 'xcode.css', 'atelier-dune.dark.css', 'atelier-lakeside.light.css', 'dark.css', 'hybrid.css', 'monokai.css', 'railscasts.css', 'tomorrow.css', 'zenburn.css', 'atelier-dune.light.css', 'atelier-seaside.dark.css', 'default.css', 'idea.css', 'monokai_sublime.css', 'rainbow.css', 'tomorrow-night-blue.css', 'atelier-forest.dark.css', 'atelier-seaside.light.css', 'docco.css', 'ir_black.css', 'obsidian.css', 'school_book.css', 'tomorrow-night-bright.css', 'atelier-forest.light.css', 'brown_paper.css', 'far.css', 'kimbie.dark.css', 'paraiso.dark.css', 'tomorrow-night.css', 'atelier-heath.dark.css', 'foundation.css', 'kimbie.light.css', 'paraiso.light.css', 'solarized_dark.css', 'tomorrow-night-eighties.css'];
 
-  $scope.makeActive = function (item) {
-    $scope.name.active = item;
+  $scope.makeActive = function (value) {
+    $scope.name.active = value;
   };
-});
 
+  chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
+    $scope.$apply(function () {
+      $scope.highlighterStyle = { active: response.highlight_css_link };
+    });
+  });
+
+  $scope.$watch('highlighterStyle.active', function (newVal, oldVal) {
+    chrome.storage.local.set({ 'highlight_css_link': newVal });
+  });
+});
 
