490fdc3b300052eac52f03c03149772f5f5dce2c
nverba <distantstate@gmail.com>
2014-08-18 16:14:29 +0100

    changes options to ng-repeat with titelize filter

--- app/options/clear-code-options.js
+++ app/options/clear-code-options.js
@@ -29,23 +29,23 @@ Options.controller('HeadController', function HeadController($scope, $timeout) {
 
 Options.controller('OptionsController', function OptionsController($scope) {
 
-  var js_beautifier_defaults = {
+  $scope.js_beautifier_defaults = {
 
-    "indent_size": 4,
-    "indent_char": " ",
-    "indent_level": 0,
-    "indent_with_tabs": false,
-    "preserve_newlines": true,
-    "max_preserve_newlines": 10,
-    "jslint_happy": false,
-    "brace_style": "collapse",
-    "keep_array_indentation": false,
-    "keep_function_indentation": false,
-    "space_before_conditional": true,
-    "break_chained_methods": false,
-    "eval_code": false,
-    "unescape_strings": false,
-    "wrap_line_length": 0
+    "indent_size": { default: 4, type: 'number', min: 1, max: 8 },
+    "indent_char": { default: " " },
+    "indent_level": { default: 0, type: 'number' },
+    "indent_with_tabs": { default: false, type: 'checkbox' },
+    "preserve_newlines": { default: true, type: 'checkbox' },
+    "max_preserve_newlines": { default: 10, type: 'number' },
+    "jslint_happy": { default: false, type: 'checkbox' },
+    "brace_style": { default: "collapse" },
+    "keep_array_indentation": { default: false, type: 'checkbox' },
+    "keep_function_indentation": { default: false, type: 'checkbox' },
+    "space_before_conditional": { default: true, type: 'checkbox' },
+    "break_chained_methods": { default: false, type: 'checkbox' },
+    "eval_code": { default: false, type: 'checkbox' },
+    "unescape_strings": { default: false, type: 'checkbox' },
+    "wrap_line_length": { default: 0, type: 'number' }
 
   };
 
@@ -54,8 +54,8 @@ Options.controller('OptionsController', function OptionsController($scope) {
   $scope.options = ['arta.css', 'atelier-heath.light.css', 'codepen-embed.css', 'github.css', 'magula.css', 'pojoaque.css', 'solarized_light.css', 'vs.css', 'ascetic.css', 'atelier-lakeside.dark.css', 'color-brewer.css', 'googlecode.css', 'mono-blue.css', 'sunburst.css', 'xcode.css', 'atelier-dune.dark.css', 'atelier-lakeside.light.css', 'dark.css', 'hybrid.css', 'monokai.css', 'railscasts.css', 'tomorrow.css', 'zenburn.css', 'atelier-dune.light.css', 'atelier-seaside.dark.css', 'default.css', 'idea.css', 'monokai_sublime.css', 'rainbow.css', 'tomorrow-night-blue.css', 'atelier-forest.dark.css', 'atelier-seaside.light.css', 'docco.css', 'ir_black.css', 'obsidian.css', 'school_book.css', 'tomorrow-night-bright.css', 'atelier-forest.light.css', 'brown_paper.css', 'far.css', 'kimbie.dark.css', 'paraiso.dark.css', 'tomorrow-night.css', 'atelier-heath.dark.css', 'foundation.css', 'kimbie.light.css', 'paraiso.light.css', 'solarized_dark.css', 'tomorrow-night-eighties.css'];
 
   $scope.resetJS = function (argument) {
-    angular.forEach(js_beautifier_defaults, function (value, key) {
-      $scope.js_beautifier[key] = value;
+    angular.forEach($scope.js_beautifier_defaults, function (value, key) {
+      $scope.js_beautifier[key] = value.default;
     });
   };
 
@@ -65,7 +65,7 @@ Options.controller('OptionsController', function OptionsController($scope) {
     });
   });
 
-  chrome.storage.local.get({ 'js_beautifier': js_beautifier_defaults }, function (response) {
+  chrome.storage.local.get({ 'js_beautifier': $scope.js_beautifier_defaults }, function (response) {
     $scope.$apply(function () {
       $scope.js_beautifier = response.js_beautifier;
     });
@@ -105,5 +105,21 @@ Options.directive('sampleCode', function () {
       elem[0].innerHTML = "//comment...! \n\nvar elements = document.getElementsByTagName('div'); \nvar sample = {}; \n\nfunction SampleFunction() { \n  console.log('hello world'); \n}";
       hljs.highlightBlock(elem[0]);
     }
+  };
+});
+
+Options.filter('titelize', function () {
+
+  return function (input, arg) {
+
+    var words = input.split('_');
+    var array = [];
+
+    for (var i=0; i<words.length; ++i) {
+      array.push(words[i].charAt(0).toUpperCase() + words[i].toLowerCase().slice(1));
+    }
+
+    return array.join(' ');
+
   };
 });
