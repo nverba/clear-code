e8792881f926531e070801259a1b20fc005d622e
nverba <distantstate@gmail.com>
2014-08-18 14:06:13 +0100

    js_beautifier working with loaded configs

--- app/clear-code-content.js
+++ app/clear-code-content.js
@@ -15,13 +15,12 @@ function openCode(element) {
 }
 
 function injectButton(element) {
-  var code   = element.getElementsByTagName('code')[0];
   var div    = document.createElement('div');
   var button = document.createElement('button');
   button.style.backgroundImage = 'url(' + chrome.extension.getURL("icons/icon19.png") + ')';
   button.className = 'clear-code';
   button.onclick = function () {
-    openCode(code);
+    openCode(element);
   };
   div.appendChild(button);
   element.appendChild(div);
--- app/clear-code-frame.js
+++ app/clear-code-frame.js
@@ -7,6 +7,32 @@ var html  = document.getElementById('beautify_html');
 var css   = document.getElementById('beautify_css');
 var reset = document.getElementById('beautify_reset');
 
+var js_beautifier;
+
+var js_beautifier_defaults = {
+
+  "indent_size": 4,
+  "indent_char": " ",
+  "indent_level": 0,
+  "indent_with_tabs": false,
+  "preserve_newlines": true,
+  "max_preserve_newlines": 10,
+  "jslint_happy": false,
+  "brace_style": "collapse",
+  "keep_array_indentation": false,
+  "keep_function_indentation": false,
+  "space_before_conditional": true,
+  "break_chained_methods": false,
+  "eval_code": false,
+  "unescape_strings": false,
+  "wrap_line_length": 0
+
+};
+
+chrome.storage.local.get({ 'js_beautifier': js_beautifier_defaults }, function (response) {
+  js_beautifier = response.js_beautifier;
+});
+
 var code  = "";
 
 function highlight() {
@@ -16,7 +42,9 @@ function highlight() {
 
 function format(beautifier) {
   code_block.innerHTML = code;
-  if (beautifier) { code_block.innerText = beautifier(code_block.innerText); }
+  if (beautifier) {
+    code_block.innerText = beautifier(code_block.innerText, js_beautifier);
+  }
   highlight();
 }
 
@@ -28,13 +56,17 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 });
 
 chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
-  hlcss.href = 'highlight/styles/' + response.highlight_css_link + '.css';
+  hlcss.href = 'highlight/styles/' + response.highlight_css_link;
 });
 
 chrome.storage.onChanged.addListener(function (changes, namespace) {
-  if (!changes.highlight_css_link) { return; }
-  hlcss.href = 'highlight/styles/' + changes.highlight_css_link.newValue + '.css';
-  highlight();
+  if (changes.highlight_css_link) {
+    hlcss.href = 'highlight/styles/' + changes.highlight_css_link.newValue;
+    highlight();
+  }
+  if (changes.js_beautifier) {
+    js_beautifier = changes.js_beautifier.newValue;
+  }
 });
 
 hide.onclick = function (event) {
