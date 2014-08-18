491ff5dd07656dad18248786e6c9aecd03752186
nverba <distantstate@gmail.com>
2014-08-18 10:14:27 +0100

    added more options - fixed css refresh bug

--- app/options/clear-code-options.js
+++ app/options/clear-code-options.js
@@ -2,6 +2,15 @@ var Options = angular.module('Options', []);
 
 Options.controller('HeadController', function HeadController($scope, $timeout) {
 
+  function refresh() {
+    // refresh each pre block to force css re-paint on mouse-up;
+    angular.forEach(document.getElementsByTagName('pre'), function (tag) {
+      $timeout(function function_name () {
+        tag.style.display = 'block';
+      }, 10);
+    });
+  }
+
   chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
     $scope.$apply(function () {
       $scope.highlighterStyle = { url: chrome.extension.getURL('highlight/styles/' + response.highlight_css_link)};
@@ -13,6 +22,7 @@ Options.controller('HeadController', function HeadController($scope, $timeout) {
 
     $scope.$apply(function () {
       $scope.highlighterStyle.url = chrome.extension.getURL('highlight/styles/' + changes.highlight_css_link.newValue);
+      refresh();
     });
   });
 });
@@ -39,9 +49,15 @@ Options.controller('OptionsController', function OptionsController($scope) {
 
   };
 
-  $scope.menu    = ['Highlighter', 'Beautifier'];
+  $scope.menu    = ['Highlighter', 'JS Beautifier', 'HTML Beautifier', 'CSS Beautifier'];
   $scope.active  = { name: $scope.menu[0] };
   $scope.options = ['arta.css', 'atelier-heath.light.css', 'codepen-embed.css', 'github.css', 'magula.css', 'pojoaque.css', 'solarized_light.css', 'vs.css', 'ascetic.css', 'atelier-lakeside.dark.css', 'color-brewer.css', 'googlecode.css', 'mono-blue.css', 'sunburst.css', 'xcode.css', 'atelier-dune.dark.css', 'atelier-lakeside.light.css', 'dark.css', 'hybrid.css', 'monokai.css', 'railscasts.css', 'tomorrow.css', 'zenburn.css', 'atelier-dune.light.css', 'atelier-seaside.dark.css', 'default.css', 'idea.css', 'monokai_sublime.css', 'rainbow.css', 'tomorrow-night-blue.css', 'atelier-forest.dark.css', 'atelier-seaside.light.css', 'docco.css', 'ir_black.css', 'obsidian.css', 'school_book.css', 'tomorrow-night-bright.css', 'atelier-forest.light.css', 'brown_paper.css', 'far.css', 'kimbie.dark.css', 'paraiso.dark.css', 'tomorrow-night.css', 'atelier-heath.dark.css', 'foundation.css', 'kimbie.light.css', 'paraiso.light.css', 'solarized_dark.css', 'tomorrow-night-eighties.css'];
+
+  $scope.resetJS = function (argument) {
+    angular.forEach(js_beautifier_defaults, function (key) {
+      $scope.js_beautifier[key] = js_beautifier_defaults[key];
+    });
+  };
 
   chrome.storage.local.get({ 'highlight_css_link': 'default' }, function (response) {
     $scope.$apply(function () {
@@ -68,8 +84,12 @@ Options.controller('OptionsController', function OptionsController($scope) {
   };
 
   $scope.$watch('highlighterStyle.active', function (newVal, oldVal) {
-    if (!$scope.highlighterStyle) return;
+    if (newVal === oldVal) return;
     chrome.storage.local.set({ 'highlight_css_link': newVal });
+  });
+
+  $scope.$watchCollection('js_beautifier', function (newVals, oldVals) {
+    chrome.storage.local.set({ 'js_beautifier': newVals });
   });
 });
 
