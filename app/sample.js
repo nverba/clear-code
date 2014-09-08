451d7a33e50582724bc196cf2f51715ead7c197f
nverba <distantstate@gmail.com>
2014-09-08 09:31:15 +0100

    added sample iframe

---
+++ app/sample.js
@@ -0,0 +1,13 @@
+hljs.initHighlightingOnLoad();
+
+var css_link = document.getElementById('css_theme');
+
+chrome.storage.local.get({ 'clearCodeOptions': { highlighter_options: { css_theme: 'default.css' }}}, function (response) {
+  css_link.href = 'highlight/styles/' + response.clearCodeOptions.highlighter_options.css_theme;
+});
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  if (namespace !== 'local' || !changes.clearCodeOptions ) { return; }
+
+  css_link.href = 'highlight/styles/' + changes.clearCodeOptions.newValue.highlighter_options.css_theme;
+});
