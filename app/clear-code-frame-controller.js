05f6b865255f29d05d87457bce9636451f527ef3
nverba <distantstate@gmail.com>
2014-09-09 02:54:20 +0100

    added margin optons for frame

--- app/clear-code-frame-controller.js
+++ app/clear-code-frame-controller.js
@@ -4,4 +4,12 @@ ClearCodeApp.controller('ClearCodeFrameController', function ClearCodeFrameContr
     chrome.runtime.sendMessage({ tabMessage: { frameDisplay: 'none' }});
   };
 
+  var container = document.getElementById('clear-code-container');
+
+  options.ready.then(function () {
+    $scope.margins = options.categories.highlighter_options.margin_size;
+  });
+
+
+
 });
