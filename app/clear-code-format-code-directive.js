b5ed3855fc230803a2b05d30d28941c669c2c462
nverba <distantstate@gmail.com>
2014-09-11 17:31:04 +0100

    updated code element to be replaced before format

--- app/clear-code-format-code-directive.js
+++ app/clear-code-format-code-directive.js
@@ -7,34 +7,35 @@ ClearCodeApp.directive('formatCode', function (options, $timeout) {
     link: function (scope, elem, attrs) {
 
       var element = elem[0],
-          code    = "",
-          pre     = document.getElementById('clear-code-pre');
+          pre     = document.getElementById('clear-code-pre'),
+          content = '',
+          saved_state = pre.innerHTML,
+          code;
 
       function highlight_code() {
 
         pre.className = scope.active_beau ? 'prettyprint lang-' + scope.active_beau : "prettyprint";
+        pre.className += options.categories.theme_options.line_nums ? ' linenums ' : '';
         prettyPrint();
       }
 
       scope.formatCode = function format_code(name) {
+        options.ready.then(function () {
 
-        element.innerText = code;
-        if (name) {
-          options.ready.then(function () {
-            element.innerText = window[name + '_beautify'](element.innerText, options.categories[name + '_options']);
-            scope.active_beau = name;
-            highlight_code();
-          });
-        } else {
-          scope.active_beau = '';
+          pre.innerHTML  = saved_state;
+          code = document.getElementById('clear-code-code');
+          code.innerText = content;
+          scope.active_beau = name ? name : false;
+
           highlight_code();
-        }
+
+        });
       };
 
       chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 
         if (request.openCode) {
-          code = JSON.parse(request.openCode);
+          content = JSON.parse(request.openCode);
           scope.formatCode();
         }
       });
