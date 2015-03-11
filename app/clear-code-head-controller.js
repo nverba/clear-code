72c03a6efddb3b13879c6ceb71ebf3cbcd993397
nverba <distantstate@gmail.com>
2015-03-11 11:22:27 +0000

    updated head controller to controller as style

--- app/clear-code-head-controller.js
+++ app/clear-code-head-controller.js
@@ -4,14 +4,14 @@
 
   function ClearCodeHeadControllerFn($scope, options) {
 
-    $scope.css = {};
-
+    this.css = {};
     options.ready.then(function () {
 
-      $scope.$watch( function (){ return options.categories.theme_options; }, function (newValue, oldValue) {
-        $scope.css.highlighter = chrome.extension.getURL( 'prettyprint/' + options.categories.theme_options.css_theme + '.css' );
-      });
-    });
+      $scope.$watch( function (){ return options.categories.theme_options; }, angular.bind(this, function () {
+        this.css.highlighter = chrome.extension.getURL( 'prettyprint/' + options.categories.theme_options.css_theme + '.css' );
+      } ));
+
+    }.bind(this));
   }
 
 })();
