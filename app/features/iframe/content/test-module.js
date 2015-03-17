ebfaeb6a0f1fb85ed6716d8e2af76459420430e8
nverba <distantstate@gmail.com>
2015-03-17 11:54:37 +0000

    more specific loading of deps to reduce zip size - unneeded files

---
+++ app/features/iframe/content/test-module.js
@@ -0,0 +1,7 @@
+(function () { 'use strict';
+
+  // This module is injected for simplicity, the testing service (injected dynamically) exposes a testrunner api to the console.
+
+  angular.module('TestModule', ['options']);
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,13 @@
-var del           = require('del');
-var gulp          = require('gulp');
-var zip           = require('gulp-zip');
-var inject        = require('gulp-inject');
-var dependencies  = Object.keys(require('./package.json').dependencies);
-var libs          = dependencies.map(function (dep) {
-                      return  './node_modules/' + dep + '/**/*.*';
-                    });
-
-var tests_libs    = ['node_modules/chai/chai.js'];
-
-var paths = {
-  testTarget: 'app/features/iframe/content/',
-  testSources: 'app/tests/**/*.js'
-};
-
-gulp.task('tests:inject', function () {
-  return gulp.src(tests_libs, { base: 'node_modules' })
-    .pipe(gulp.dest('app/tests'));
-});
-
-gulp.task('tests:flush', ['tests:unlink'], function (cb) {
-  del(['app/tests'], cb);
-});
-
-gulp.task('tests:insert', ['tests:inject'], function () {
-
-  return gulp.src(paths.testTarget + 'index.html')
-    .pipe(inject(gulp.src(paths.testSources, {read: false}), {relative: true}))
-    .pipe(gulp.dest(paths.testTarget));
-});
-
-gulp.task('tests:unlink', function () {
-
-  return gulp.src(paths.testTarget + 'index.html')
-    .pipe(inject(gulp.src(paths.testSources, {read: false}), {
-      transform: function (filePath, file) {
-        return '';
-      }
-    }))
-    .pipe(gulp.dest(paths.testTarget));
-});
+var del     = require('del');
+var gulp    = require('gulp');
+var zip     = require('gulp-zip');
+var libs    = [
+                './node_modules/angular/angular.js',
+                './node_modules/angular-chrome-options/dist/**/*.*',
+                './node_modules/chrome-bootstrap/chrome-bootstrap.css',
+                './node_modules/google-code-prettify/bin/prettify.min.js',
+                './node_modules/js-beautify/js/lib/**/*.*'
+              ];
 
 gulp.task('app:clean', function (cb) {
   del(['app/libs/**'], cb);
@@ -57,4 +25,4 @@ gulp.task('zip', ['test:flush'], function () {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['karma']);
+gulp.task('default', []);
