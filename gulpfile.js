a359ea93abe91fd78780584fd7c33d7a112028a8
nverba <distantstate@gmail.com>
2015-03-17 01:37:25 +0000

    added test insertion and linking

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var del           = require('del');
 var gulp          = require('gulp');
 var zip           = require('gulp-zip');
+var inject        = require('gulp-inject');
 var dependencies  = Object.keys(require('./package.json').dependencies);
 var libs          = dependencies.map(function (dep) {
                       return  './node_modules/' + dep + '/**/*.*';
@@ -8,13 +9,36 @@ var libs          = dependencies.map(function (dep) {
 
 var tests_libs    = ['node_modules/chai/chai.js'];
 
-gulp.task('test:insert', function (cb) {
-  return gulp.src(libs, { base: 'node_modules' })
-    .pipe(gulp.dest('app/libs'));
+var paths = {
+  testTarget: 'app/features/iframe/content/',
+  testSources: 'app/tests/**/*.js'
+};
+
+gulp.task('tests:inject', function () {
+  return gulp.src(tests_libs, { base: 'node_modules' })
+    .pipe(gulp.dest('app/tests'));
 });
 
-gulp.task('test:flush', function (cb) {
+gulp.task('tests:flush', ['tests:unlink'], function (cb) {
+  del(['app/tests'], cb);
+});
 
+gulp.task('tests:insert', ['tests:inject'], function () {
+
+  return gulp.src(paths.testTarget + 'index.html')
+    .pipe(inject(gulp.src(paths.testSources, {read: false}), {relative: true}))
+    .pipe(gulp.dest(paths.testTarget));
+});
+
+gulp.task('tests:unlink', function () {
+
+  return gulp.src(paths.testTarget + 'index.html')
+    .pipe(inject(gulp.src(paths.testSources, {read: false}), {
+      transform: function (filePath, file) {
+        return '';
+      }
+    }))
+    .pipe(gulp.dest(paths.testTarget));
 });
 
 gulp.task('app:clean', function (cb) {
